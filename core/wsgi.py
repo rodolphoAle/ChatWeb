41da98eec3a5c7b12d7e72aa22819089b62272e8
seraeac <rodolphosilva.ale@gmail.com>
2024-09-16 18:25:17 -0300

    Instalando as depedencia do projeto. django cors, djangorestframework-simplejwt, markdown, djangorestframewo, socketio. Realizando o controle de acesso de token

--- core/wsgi.py
+++ core/wsgi.py
@@ -1,16 +1,16 @@
-"""
-WSGI config for core project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
-
 import os
+import eventlet.wsgi
+import socketio
+import eventlet
 
 from django.core.wsgi import get_wsgi_application
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+
+from core.socket import socket
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
-application = get_wsgi_application()
+application = StaticFilesHandler(get_wsgi_application())
+application = socketio.WSGIApp(socket, application)
+
+eventlet.wsgi.server(eventlet.listen(('', 8000)), application)
