258d1994a3269a095b2e342e201684f50b156f92
seraeac <rodolphosilva.ale@gmail.com>
2024-09-16 17:00:13 -0300

    criando a conecção com o banco mysql e fazendo ouvir na porta 3306

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=E:\Dowloads Void\faculdade\PROG WEB\Chat-Web\BACKEND\venv
+set VIRTUAL_ENV=E:\Dowloads Void\faculdade\PROG WEB\Chat-Web\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
